e12debcf176470b07d382c0fa56499cc00e21b93
VictorAnnell <30280491+VictorAnnell@users.noreply.github.com>
2023-11-20 14:16:53 +0100

    Put collector logic into a class

--- app/main.py
+++ app/main.py
@@ -4,14 +4,22 @@ import os
 import json
 from nats.errors import ConnectionClosedError, TimeoutError, NoServersError
 
+class Collector:
+    @classmethod
+    async def create(cls):
+        self = cls()
+        self.modules = []
+        self.nats_url = os.getenv("BROKER_URL", "nats://localhost:4222")
+        self.nc = await nats.connect(self.nats_url)
+        return self
+
+    async def send(self):
+        await self.nc.publish("updates", json.dumps({"id": "1", "module": "any module", "data": "nice data",}).encode())
+
+
 async def main():
-    nats_url = os.getenv("BROKER_URL", "nats://localhost:4222")
-    nc = await nats.connect(nats_url)
-
-    # wait for data
-    # put data into json format
-
-    await nc.publish("updates", json.dumps({"id": "1", "module": "any module", "data": "nice data",}).encode())
+    collector = await Collector.create()
+    await collector.send()
 
 if __name__ == "__main__":
     asyncio.run(main())
