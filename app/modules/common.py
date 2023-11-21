f9de510f892db846ac3d390178504407604ab901
VictorAnnell <30280491+VictorAnnell@users.noreply.github.com>
2023-11-21 17:33:06 +0100

    Update network_traffic module to new interface

--- app/modules/common.py
+++ app/modules/common.py
@@ -7,6 +7,7 @@ import nats
 class BaseCollector:
     def __init__(self, nc):
         self.nc = nc
+        self.data = []
 
     async def send(self, module, data):
         await self.nc.publish("updates", json.dumps({"id": "1", "module": module, "data": data, }).encode())
--- app/modules/network_traffic.py
+++ app/modules/network_traffic.py
@@ -1,17 +1,28 @@
 import pyshark
 from modules.common import BaseCollector
 import sys
+import asyncio
+from asyncer import asyncify
 
 class NetworkTrafficCollector(BaseCollector): 
-
-    def __init__(self):
-        super().__init__()
-
     def collect(self):
-        capture =  pyshark.LiveCapture(interface='wlo1')
-        for packet in capture.sniff_continuously(packet_count=5):
+        capture =  pyshark.LiveCapture(interface='wlan0')
+        for packet in capture.sniff_continuously(packet_count=1):
             self.data.append(str(packet))
-    
-    async def publish(self):
-        await super().publish()
-        return
+
+    async def contious_send(self):
+            for point in self.data:
+                print(point)
+                await self.send("network_traffic", point)
+                self.data.remove(point)
+
+    async def run(self):
+        while True:
+            await asyncify(self.collect)()
+            await self.contious_send()
+
+            # await asyncio.sleep(1)
+
+        # async def publish(self):
+        #     for point in self.data:
+        #         await self.nc.publish("updates", json.dumps({"id": "1", "module": "any module", "data": point,}).encode())
