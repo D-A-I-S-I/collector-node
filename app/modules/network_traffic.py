ba7f426bbfc464b5ec7a73beecf3b976a7ae57bc
VictorAnnell <30280491+VictorAnnell@users.noreply.github.com>
2023-12-21 16:44:52 +0100

    Fix/update network_traffic collector

--- app/modules/network_traffic.py
+++ app/modules/network_traffic.py
@@ -1,17 +1,23 @@
-import json
-import pyshark
-import jspcap
-import subprocess
-import os
-from modules.common import BaseCollector
-import sys
 import asyncio
-from asyncer import asyncify
+import json
+import os
 import shlex
+import subprocess
+import sys
+
+import jspcap
+import pyshark
+from asyncer import asyncify
+from modules.common import BaseCollector
 
 
-class NetworkTrafficCollector(BaseCollector): 
+class NetworkTrafficCollector(BaseCollector):
     module_name = "network_traffic"
+
+    def __init__(self, nc):
+        super().__init__(nc)
+        self.interface = os.getenv("NETWORK_TRAFFIC_INTERFACE", "wlan0")
+        self.packet_count = os.getenv("NETWORK_TRAFFIC_PACKET_COUNT", "1000")
 
     def print_callback(pkt):
         print("ho")
@@ -20,7 +26,7 @@ class NetworkTrafficCollector(BaseCollector):
         #self.data.append(pkt)
 
     def transform(self,inputfile):
-        #command = "../FlowMeter/pkg/flowmeter -ifLiveCapture=false -fname=packet -maxNumPackets=40000000 -ifLocalIPKnown false INFO[0000] Liv" 
+        #command = "../FlowMeter/pkg/flowmeter -ifLiveCapture=false -fname=packet -maxNumPackets=40000000 -ifLocalIPKnown false INFO[0000] Liv"
         #command = "tshark -r packet.pcap -T json -x > packet.json"
         #formatted_command = shlex.split(command)
         #subprocess.run(formatted_command)
@@ -29,30 +35,48 @@ class NetworkTrafficCollector(BaseCollector):
                 os.path.join(inputfile),
                 "-T", "json", "-x"],
                 stdout=outfile, check=True)
-               
-    
-    def collect(self):
-        file = "packet.pcap"
-        output = open(file, "w")
-        print(type(output))
-        capture = pyshark.LiveCapture(interface="wlo1", output_file=file)
-        capture.sniff(packet_count=15000)
-        output.close()
-        self.transform(file)
 
-        with open("outfile.json", 'r') as openfile:
-            json_object = json.load(openfile)
-        self.data.append(json_object)
-        return
+
+    async def collect(self):
+        command = ["tshark", "-i", self.interface, "-T", "json", "-x", "-c", str(self.packet_count)]
+
+        # Create subprocess
+        process = await asyncio.create_subprocess_exec(
+            *command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
+
+        # Wait for the subprocess to finish
+        stdout, stderr = await process.communicate()
+
+        if process.returncode == 0:
+            # Process completed successfully, parse JSON output
+            try:
+                json_data = json.loads(stdout.decode())
+                self.data.append(json_data)
+            except json.JSONDecodeError as e:
+                print(f"Failed to decode JSON from process output: {e}")
+        else:
+            # Process failed, handle errors
+            print(f"Command failed with exit code {process.returncode}")
+            print(f"stderr: {stderr.decode().strip()}")
 
     async def run(self):
         while True:
-            await asyncify(self.collect)()
+            await self.collect()
             await self.publish()
+        return
 
     async def publish(self):
-        # for point in self.data:
-        await self.nc.publish("updates", json.dumps({"id": "1", "module": "network_traffic", "data": self.data,}).encode())
+        to_remove = []
+        if self.data:
+            for point in self.data:
+                await self.send(point)
+                to_remove.append(point)
+
+        for point in to_remove:
+            self.data.remove(point)
 
     def collect_flow(self):
         command = "sysdig -p'%evt.num %evt.arg'" #I think it must be a number in here but did not found the field yet
